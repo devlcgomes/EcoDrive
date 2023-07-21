661e9a9337a44d3f709045c465de08a6329220ea
devlcgomes <dev.lucianogomes@gmail.com>
2023-07-21 19:48:26 -0300

    Configure view binding

--- app/src/main/java/devandroid/gomes/ecodrive/MainActivity.kt
+++ app/src/main/java/devandroid/gomes/ecodrive/MainActivity.kt
@@ -2,10 +2,18 @@ package devandroid.gomes.ecodrive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import devandroid.gomes.ecodrive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Instanciando o binding com o inflate
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // Agora eu seto o meu contentLayout como root
+        setContentView(binding.root)
+
+
     }
 }
